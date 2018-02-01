eb959c2ae33480e809dc164b0cf4a441392d1161
Charles Langevin <varicelle3@hotmail.ca>
2018-02-01 14:27:12 -0500

    Ajout des données dans le Main

--- TP1_AssuranceQualite/src/CV.java
+++ TP1_AssuranceQualite/src/CV.java
@@ -1,9 +1,35 @@
 
 public class CV {
 
+	private String nom;
+	private String prenom;
+	private String formation;
+	private int exp;
+	private String[] competence;
+	private String attente;
+	
+	public CV (String nom, String prenom, String formation, int exp, String[] competence, String attente) {
+		this.nom = nom;
+		this.prenom = prenom;
+		this.formation = formation;
+		this.exp = exp;
+		this.competence = competence;
+		this.attente = attente;
+	}
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		String[] competenceCharles = {"Patient", "Logique"};
+		String[] competencePhilippe = {"debrouillard", "adapte facilement", "apprend facilement"};
+		CV charles = new CV("Langevin","Charles","Informatique",0,competenceCharles,"Le passer");
+		CV philippe = new CV("philippe","chevry","informatique",0,competencePhilippe,"apprendre de nouvelle chose");
+		
+		charles.afficher();
+		philippe.afficher();
+	}
+	
+	public void afficher() {
+		
 	}
 
 }
