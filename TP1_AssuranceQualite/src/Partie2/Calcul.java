64361bb2b7c375a42ed11a142c563fa736de82ff
Charles Langevin <charles.langevin@hotmail.com>
2018-02-12 09:40:12 -0500

    Calcul terminer

--- TP1_AssuranceQualite/src/Partie2/Calcul.java
+++ TP1_AssuranceQualite/src/Partie2/Calcul.java
@@ -2,14 +2,50 @@ package Partie2;
 
 public class Calcul {
 	
+	public String[] clients = {"Roger","Céline","Steeve"};
+	private String[][] plats = {{"Poutine","10.5"},{"Frites","2.5"},{"Repas_Poulet","15.75"}};
+	private String[][] commandes = {{"Roger","Poutine","1"},{"Céline","Frites","2"},{"Céline","Repas_Poulet","1"}};;
+	
+	public double[] prix;
 	
 	public Calcul() {
+		prix = new double[clients.length];
 		
+		faireCalcul();
 	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		new Calcul();
 	}
-
+	
+	private void faireCalcul() {
+		double resultatPrix = 0;
+		
+		for(int i = 0; i < clients.length; i++) {
+			String client = clients[i];
+			
+			for(int j = 0; j < commandes.length; j++) {
+				if(client.equals(commandes[j][0])) {
+					String plat = commandes[j][1];
+					
+					for(int k = 0; k < plats.length; k++) {
+						if(plat.equals(plats[k][0])) {
+							try {
+								double nb = Double.parseDouble(commandes[j][2]);
+								double prixUnit = Double.parseDouble(plats[k][1]);
+								resultatPrix = nb * prixUnit;
+							}catch(NumberFormatException e) {
+								System.out.print("Erreur calcul");
+							}				
+						}
+					}
+				}
+				prix[i] += resultatPrix;
+				resultatPrix = 0;
+			}			
+		}
+		
+		new Facture(clients, prix);
+	}
 }
--- TP1_AssuranceQualite/src/Partie2/Facture.java
+++ TP1_AssuranceQualite/src/Partie2/Facture.java
@@ -1,12 +1,27 @@
 package Partie2;
 
-public class Facture {
+public class Facture{
 	
-	public Facture(){
-		
+	java.text.DecimalFormat df = new java.text.DecimalFormat("0.00$");
+	
+	private String[] clients;
+	
+	private double[] prix;
+	
+	public Facture(String[] clientsM, double[] prixM){
+		this.clients = clientsM;
+		this.prix = prixM;	
+		afficher();
 	}
 	
 	public void afficher(){
+		
+		System.out.println("Bienvenue chez Barette!");
+		System.out.println("Factures:");
+		
+		for(int i = 0; i < clients.length; i++) {
+			System.out.println(clients[i] + " " + df.format(prix[i]));
+		}
 		
 	}
 
